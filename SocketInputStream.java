03c91c956c121173b00d0d586d9069a24d4536e9
LeeHansome <yonzhangli@gmail.com>
2016-11-19 22:55:16 +0800

    Add files via upload

--- SocketInputStream.java
+++ SocketInputStream.java
@@ -1,5 +1,6 @@
 package com.hsm.connector.http;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,6 +14,7 @@ public class SocketInputStream extends InputStream {
 	
 	protected byte buf[];
 	protected int count;
+	protected int pos;
 	protected InputStream is;
 	
 	public SocketInputStream(InputStream is,int bufferSize){
@@ -21,8 +23,56 @@ public class SocketInputStream extends InputStream {
 	}
 	/*protected static StringManager sm =
 	        StringManager.getManager(Constants.Package);*/
-	public void readRequestLine(HttpRequestLine requestLine){
+	public void readRequestLine(HttpRequestLine requestLine) throws IOException{
+		if(requestLine.methodEnd != 0)
+			requestLine.recycle();
+		int chr = 0;
+		do{
+			try{
+				chr = read();
+			}catch(IOException e){
+				chr = -1;
+			}
+		}while((chr == CR)||(chr == LF));
+		//if(chr == -1)			
+		//	throw new EOFException(sm.getString("requestStream.readline.error"));
+		pos--;
 		
+		//Read the method
+		
+		int maxRead = requestLine.method.length;
+		int readStart = pos;
+		int readCount = 0;
+		boolean space = false;
+		while(!space){
+			if(readCount >= maxRead){
+				if((2 * maxRead) <= HttpRequestLine.MAX_METHOD_SIZE){
+					char[] newBuffer = new char[2 * maxRead];
+					System.arraycopy(requestLine.method , 0, newBuffer,0, maxRead);
+					requestLine.method = newBuffer;
+					maxRead = requestLine.method.length;
+				}else{
+					/* throw new IOException
+                     (sm.getString("requestStream.readline.toolong"));*/
+				}
+			}			
+			if(pos >= count){
+				int val = read();
+				if(val == -1){
+					/* throw new IOException
+                       (sm.getString("requestStream.readline.toolong"));*/
+				}					
+				pos = 0;
+				readStart = 0;
+			}
+			if(buf[pos]==SP){
+				space = true;
+			}
+			requestLine.method[readCount] = (char) buf[pos];
+			readCount++;
+			pos++;
+		}
+		requestLine.methodEnd = readCount - 1;
 	}
 	
 	/*public void readHeader(HttpHeader header){
@@ -41,13 +91,22 @@ public class SocketInputStream extends InputStream {
 		buf = null;
 	}
 	
-	protected void fill(){
-		
+	protected void fill() throws IOException{
+		pos = 0;
+		count = 0;
+		int nRead = is.read(buf, 0, buf.length);
+		if(nRead > 0)
+			count = nRead;
 	}
 	
 	@Override
 	public int read() throws IOException {
-		return 0;
+		if(pos >= count){
+			fill();
+			if(pos >= count)
+				return -1;
+		}
+		return buf[pos++] & 0xff;
 	}
 
 }
